e350b6c6af6b6af9ef1d84b1dd3d41b079274320
Caio <caiodesouza.cds@gmail.com>
2026-05-28 18:07:54 -0300

    feat: cria dto de response do usuario

---
+++ src/main/java/com/api/mygym/domain/user/dto/UserResponse.java
@@ -0,0 +1,17 @@
+package com.api.mygym.domain.user.dto;
+
+import com.api.mygym.domain.user.User;
+
+import java.util.UUID;
+
+public record UserResponse(
+
+        UUID id,
+        String name,
+        String email
+) {
+
+    public UserResponse(User user){
+        this(user.getId(), user.getName(), user.getEmail());
+    }
+}
