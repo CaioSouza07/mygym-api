3b553aac0cdc3e34f81e54c995f7187614f3e45e
Caio <caiodesouza.cds@gmail.com>
2026-05-28 18:11:04 -0300

    feat: cria dto para login de usuario

---
+++ src/main/java/com/api/mygym/domain/user/dto/UserLoginRequest.java
@@ -0,0 +1,15 @@
+package com.api.mygym.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginRequest(
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
