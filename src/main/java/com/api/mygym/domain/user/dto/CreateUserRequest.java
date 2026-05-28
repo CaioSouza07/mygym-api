4c3102512a110ad946bc09a90716b9eb4287ea54
Caio <caiodesouza.cds@gmail.com>
2026-05-28 18:05:35 -0300

    feat: cria entidade de usuario e dto para criar

---
+++ src/main/java/com/api/mygym/domain/user/dto/CreateUserRequest.java
@@ -0,0 +1,18 @@
+package com.api.mygym.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserRequest(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password
+) {
+}
