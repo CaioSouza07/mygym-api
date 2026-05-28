ed7e352059e2524a1aaf9d051adcddf4af0c2b70
Caio <caiodesouza.cds@gmail.com>
2026-05-28 19:01:27 -0300

    fix: ajuste no tipo do enum na tabela de treino

--- src/main/resources/db/migration/V3__create-table-trainings.sql
+++ src/main/resources/db/migration/V3__create-table-trainings.sql
@@ -2,7 +2,7 @@ CREATE TABLE trainings (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL,
     name VARCHAR(150) NOT NULL,
-    week_day WEEK_DAY NOT NULL,
+    week_day WEEK_DAYS NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_training_user
         FOREIGN KEY (user_id)
