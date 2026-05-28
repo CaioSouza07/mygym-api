a9bd6100bf120a7126fd068dc6179ad73c827e8e
Caio <caiodesouza.cds@gmail.com>
2026-05-28 20:05:55 -0300

    feat: cria metodo para deletar treino e exception not found

--- src/main/java/com/api/mygym/domain/training/TrainingService.java
+++ src/main/java/com/api/mygym/domain/training/TrainingService.java
@@ -3,9 +3,12 @@ package com.api.mygym.domain.training;
 import com.api.mygym.domain.training.dto.CreateTrainingRequest;
 import com.api.mygym.domain.training.dto.TrainingResponse;
 import com.api.mygym.domain.user.User;
+import com.api.mygym.infra.exception.NotFoundException;
 import com.api.mygym.infra.exception.TrainingAlreadyExistsForDayException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,13 @@ public class TrainingService {
         repository.save(training);
 
         return new TrainingResponse(training);
+    }
+
+    public void remove(UUID id){
+
+        var training = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Treino não encontrado com esse id"));
+
+        repository.delete(training);
     }
 }
