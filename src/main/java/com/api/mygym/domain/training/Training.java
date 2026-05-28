be2b786dc35a07acc0e957c6bdc46cb940cd34cb
Caio <caiodesouza.cds@gmail.com>
2026-05-28 18:50:08 -0300

    feat: cria entidade de treino e enum de semana

---
+++ src/main/java/com/api/mygym/domain/training/Training.java
@@ -0,0 +1,39 @@
+package com.api.mygym.domain.training;
+
+import com.api.mygym.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "trainings")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Training {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "week_day", nullable = false)
+    private WeekDay weekDay;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/api/mygym/domain/training/WeekDay.java
@@ -0,0 +1,11 @@
+package com.api.mygym.domain.training;
+
+public enum WeekDay {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
