30fa73bd0e6d834e0c86b8e5f9b529fc652b3f02
Caio <caiodesouza.cds@gmail.com>
2026-05-30 19:14:58 -0300

    feat: cria configuracao do security e exception

---
+++ src/main/java/com/api/mygym/infra/exception/SecurityException.java
@@ -0,0 +1,7 @@
+package com.api.mygym.infra.exception;
+
+public class SecurityException extends RuntimeException {
+    public SecurityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/api/mygym/infra/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.api.mygym.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        try {
+            return http.csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(authorize -> {
+                            authorize.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                            authorize.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                            authorize.anyRequest().authenticated();
+                        }
+                    ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                    .build();
+        } catch (Exception e) {
+            throw new SecurityException("Erro interno na configuração de segurança");
+        }
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+        try {
+            return authenticationConfiguration.getAuthenticationManager();
+        } catch (Exception e) {
+            throw new SecurityException("Erro interno na configuração de segurança");
+        }
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
