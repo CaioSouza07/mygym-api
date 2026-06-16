a2595e7fae1d0733889fef8b6d7faa7d02176dab
Caio de Souza <caiodesouza.cds@gmail.com>
2026-06-16 13:33:13 -0300

    fix: ajusta env e servico do email

--- src/main/java/com/api/mygym/infra/email/EmailService.java
+++ src/main/java/com/api/mygym/infra/email/EmailService.java
@@ -4,7 +4,6 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -57,8 +56,7 @@ public class EmailService {
 
         helper.setText(html, true);
 
-        ClassPathResource logo =
-                new ClassPathResource("static/images/mygym_logo.png");
+        ClassPathResource logo = new ClassPathResource("static/images/mygym_logo.png");
 
         helper.addInline("logoMyGym", logo);
 
