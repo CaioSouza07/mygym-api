db7a78f084b92ecea04e2e1c221b43345aec51d0
Caio de Souza <caiodesouza.cds@gmail.com>
2026-06-24 13:11:17 -0300

    fix: ajusta response de treinos, retornando a data de criação

--- src/main/java/com/api/mygym/domain/training/dto/TrainingResponse.java
+++ src/main/java/com/api/mygym/domain/training/dto/TrainingResponse.java
@@ -4,6 +4,7 @@ import com.api.mygym.domain.exercise.dto.ExerciseResponse;
 import com.api.mygym.domain.training.Training;
 import com.api.mygym.domain.training.WeekDay;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,9 +13,10 @@ public record TrainingResponse(
         UUID id,
         String name,
         WeekDay weekDay,
-        List<ExerciseResponse> exercises
+        List<ExerciseResponse> exercises,
+        LocalDateTime createdAt
 ) {
     public TrainingResponse(Training training){
-        this(training.getId(), training.getName(), training.getWeekDay(), training.getExercises().stream().map(ExerciseResponse::new).toList());
+        this(training.getId(), training.getName(), training.getWeekDay(), training.getExercises().stream().map(ExerciseResponse::new).toList(), training.getCreatedAt());
     }
 }
